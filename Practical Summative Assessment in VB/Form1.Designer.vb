d7483897f31d48267d16ae7cdfdee57a322614eb
Sanele <Sanele@LAPTOP-FC04BNU2>
2024-01-19 17:29:35 +0200

    Add project files.

---
+++ Practical Summative Assessment in VB/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        GroupBox1 = New GroupBox()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        Label2 = New Label()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 23
+        ListBox1.Location = New Point(103, 149)
+        ListBox1.Margin = New Padding(4, 5, 4, 5)
+        ListBox1.Name = "ListBox1"
+        ListBox1.ScrollAlwaysVisible = True
+        ListBox1.Size = New Size(321, 142)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Sitka Small", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(123, 25)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(193, 23)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter  Animal`s Name:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(123, 53)
+        TextBox1.Margin = New Padding(4, 5, 4, 5)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(301, 26)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.FlatAppearance.BorderColor = Color.Purple
+        Button1.FlatStyle = FlatStyle.System
+        Button1.Location = New Point(168, 89)
+        Button1.Margin = New Padding(4, 5, 4, 5)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(181, 35)
+        Button1.TabIndex = 3
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(RadioButton4)
+        GroupBox1.Location = New Point(320, 440)
+        GroupBox1.Margin = New Padding(4, 5, 4, 5)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Padding = New Padding(4, 5, 4, 5)
+        GroupBox1.Size = New Size(139, 112)
+        GroupBox1.TabIndex = 4
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Colour"
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(10, 40)
+        RadioButton3.Margin = New Padding(4, 5, 4, 5)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(103, 27)
+        RadioButton3.TabIndex = 6
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Dark Blue"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(9, 78)
+        RadioButton4.Margin = New Padding(4, 5, 4, 5)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(77, 27)
+        RadioButton4.TabIndex = 7
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Purple"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton2)
+        GroupBox2.Controls.Add(RadioButton1)
+        GroupBox2.Location = New Point(103, 440)
+        GroupBox2.Margin = New Padding(4, 5, 4, 5)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Padding = New Padding(4, 5, 4, 5)
+        GroupBox2.Size = New Size(144, 112)
+        GroupBox2.TabIndex = 5
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Case"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(8, 78)
+        RadioButton2.Margin = New Padding(4, 5, 4, 5)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(99, 27)
+        RadioButton2.TabIndex = 6
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "To Lower"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(10, 41)
+        RadioButton1.Margin = New Padding(4, 5, 4, 5)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(97, 27)
+        RadioButton1.TabIndex = 6
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "To Upper"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.BackColor = SystemColors.ButtonHighlight
+        Label2.BorderStyle = BorderStyle.Fixed3D
+        Label2.Location = New Point(70, 335)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(436, 64)
+        Label2.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 23F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightCoral
+        ClientSize = New Size(556, 582)
+        Controls.Add(Label2)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(ListBox1)
+        Font = New Font("Sitka Small", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ForeColor = Color.Black
+        Margin = New Padding(4, 5, 4, 5)
+        Name = "Form1"
+        Text = "Words"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Practical Summative Assessment in VB/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedIndex <> -1 Then
+            Label2.Text = ListBox1.SelectedItem.ToString()
+        Else
+            Label2.Text = "no item selected"
+
+
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not ListBox1.Items.Contains(TextBox1.Text) Then
+            ListBox1.Items.Add(TextBox1.Text)
+
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked Then
+            Label2.Text = Label2.Text.ToUpper()
+        End If
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        If RadioButton3.Checked Then
+            Label2.ForeColor = Color.DarkBlue()
+        End If
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If RadioButton2.Checked Then
+            Label2.Text = Label2.Text.ToLower()
+        End If
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        If RadioButton4.Checked Then
+            Label2.ForeColor = Color.Purple()
+        End If
+    End Sub
+End Class
